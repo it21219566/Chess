c6ea6862d7f7fcff53caa15f1d6c78ef9838733d
KULANA THATHSARA <it21219566@my.sliit.lk>
2025-01-23 04:41:10 +0530

    board and pieces graphics added

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -2,7 +2,6 @@
 main driver file
 responsible for handling user inputs and displaying the current GameState objects.
 """
-
 import pygame as p
 from Chess import ChessEngine
 
@@ -11,3 +10,64 @@ DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
+
+'''
+Initiate the images
+'''
+def load_images():
+    pieces = ['wR', 'wN', 'wB', 'wQ', 'wK', 'wP', 'bR', 'bN', 'bB', 'bQ', 'bK', 'bP']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+'''
+the main
+responsible for handling user inputs and graphics
+'''
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    #print(gs.board)
+    load_images() # executed once before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        draw_game_state(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+responsible for all the graphics on the current gameState
+'''
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    draw_pieces(screen, gs.board)
+
+'''
+order of drawing 01
+responsible for drawing the squares in the board
+'''
+def draw_board(screen):
+    colours = [p.Color("white"), p.Color("light blue")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            colour = colours[((r+c) % 2)]
+            p.draw.rect(screen, colour, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+''''
+order of drawing 02
+responsible for drawing the pieces on the board using current gameState.board
+'''
+def draw_pieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #if the square is not empty
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == '__main__':
+    main()
