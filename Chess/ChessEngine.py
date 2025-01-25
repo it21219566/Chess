bdd830669188549c16eb146ad9e9642033f9f700
KULANA THATHSARA <it21219566@my.sliit.lk>
2025-01-25 07:12:19 +0530

    basic move logic added

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -55,7 +55,7 @@ class GameState():
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[r][c][1]
                     self.move_functions[piece](r, c, moves) #calls the appropriate move_function based on piece type
         return moves
@@ -75,38 +75,98 @@ class GameState():
             if c+1 <= 7: #captures to the right
                 if self.board[r-1][c+1][0] == 'b': #if there is an opponents piece to capture
                     moves.append(Move((r, c), (r-1, c+1), self.board))
-        else: #black pawn moves
-            pass
+
+        else:  # Black pawn moves
+            if self.board[r + 1][c] == '--':  # 1 square pawn move
+                moves.append(Move((r, c), (r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == '--':  # 2 square pawn move
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+            if c - 1 >= 0:  # Captures to the left
+                if self.board[r + 1][c - 1][0] == 'w':  # If there is an opponent's piece to capture
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c + 1 <= 7:  # Captures to the right
+                if self.board[r + 1][c + 1][0] == 'w':  # If there is an opponent's piece to capture
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
 
     '''
     get all the rook moves for pawn located in a row, col and add those moves to the list
     '''
     def get_rook_moves(self, r, c, moves):
-        pass
+        directions = ((-1, 0), (0, -1), (1, 0), (0, 1)) # up, down, left and right
+        opponent_colour = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < len(self.board) and 0 <= end_col < len(self.board[0]): #within the board
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--": #valid empty space
+                        moves.append(Move((r, c),(end_row, end_col), self.board))
+                    elif end_piece[0] == opponent_colour: #opponents piece valid
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                    else: #friendly piece invalid
+                        break
+                else: #off the board
+                    break
 
     '''
     get all the bishop moves for pawn located in a row, col and add those moves to the list
     '''
     def get_bishop_moves(self, r, c, moves):
-        pass
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1)) #4 diaganols
+        opponent_colour = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < len(self.board) and 0 <= end_col < len(self.board[0]): #within the board
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--": #valid empty space
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == opponent_colour: #opponents piece valid
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                    else: #friendly piece invalid
+                        break
+                else: #off the board
+                    break
 
     '''
     get all the knight moves for pawn located in a row, col and add those moves to the list
     '''
     def get_knight_moves(self, r, c, moves):
-        pass
+        knight_moves = ((-2,-1), (-2,1), (-1,-2), (-1,2), (1,-2), (1,2), (2,-1), (2,1))
+        allay_colour = "w" if self.whiteToMove else "b"
+        for m in knight_moves:
+            end_row = r + m[0]
+            end_col = c + m[1]
+            if 0 < end_row < len(self.board) and 0 < end_col < len(self.board[0]):
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != allay_colour: #not an allay piece (an opponents piece or empty)
+                    moves.append(Move((r, c),(end_row, end_col),  self.board))
 
     '''
     get all the king moves for pawn located in a row, col and add those moves to the list
     '''
     def get_king_moves(self, r, c, moves):
-        pass
+        king_moves = ((-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1))
+        allay_colour = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            end_row = r + king_moves[i][0]
+            end_col = c + king_moves[i][1]
+            if 0 < end_row < len(self.board) and 0 < end_col < len(self.board[0]):
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != allay_colour:
+                    moves.append(Move((r, c),(end_row, end_col), self.board))
 
     '''
     get all the queen moves for pawn located in a row, col and add those moves to the list
     '''
     def get_queen_moves(self, r, c, moves):
-        pass
+        #essentially queen move is a combination of rook moves and bishop moves
+        self.get_rook_moves(r, c, moves)
+        self.get_bishop_moves(r, c, moves)
 
 
 class Move():
